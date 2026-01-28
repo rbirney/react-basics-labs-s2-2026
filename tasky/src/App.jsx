4d4b7426dc88fc985da4d8001e451a5601b5da94
rbirney <rbirney@wit.ie>
2026-01-28 12:43:40 +0000

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -19,6 +19,12 @@ function App() {
     //console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
   
   return (
     <div className="container">
@@ -32,6 +38,7 @@ function App() {
           key={task.id}
           done={task.done}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
         />
       ))}
 
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -9,6 +9,7 @@ const Task = (props) => {
             <p className="priority">{props.priority}</p>
             
             <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
 
         </div>
     )
