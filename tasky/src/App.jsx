188e71ba1d28b8211dda3366e78fc25fa6a1c42e
rbirney <rbirney@wit.ie>
2026-01-28 12:50:37 +0000

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
 import React, { useState } from 'react';
 import AddTaskForm from './components/Form';
 import Task from './components/Task';
@@ -13,6 +14,44 @@ function App() {
     ]
   });
   
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  }
+  
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+
+
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
@@ -43,7 +82,8 @@ function App() {
         />
       ))}
 
-      <AddTaskForm /> 
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+
     </div>
   );
 
